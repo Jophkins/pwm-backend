115440022dad5c6a92474d0aa3e5160fe7a736af
serhiilaptop <sergebrew@gmail.com>
2023-05-01 23:35:36 -0600

    cors

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -2,6 +2,21 @@ import PostModel from '../models/Post.js';
 import post from "../models/Post.js";
 
 
+export const getLastTags = async (req, res) => {
+  try {
+    const posts = await PostModel.find().limit(5).exec();
+
+    const tags = posts.map(obj => obj.tags).flat().slice(0, 5);
+
+    res.json(tags);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: 'Unable to get posts'
+    });
+  }
+}
+
 export const getAll = async (req, res) => {
   try {
     const posts = await PostModel.find().populate('user').exec();
@@ -19,17 +34,13 @@ export const getOne = async (req, res) => {
   try {
     const postId = req.params.id;
 
-    const updatedDocument = await PostModel.findOneAndUpdate(
-      {
-        _id: postId
-      },
-      {
-        $inc: {viewsCount: 1}
-      },
-      {
-        new: true
-      }
-    );
+    const updatedDocument = await PostModel.findOneAndUpdate({
+      _id: postId
+    }, {
+      $inc: {viewsCount: 1}
+    }, {
+      new: true
+    });
 
     if (!updatedDocument) {
       return res.status(404).json({
@@ -39,8 +50,7 @@ export const getOne = async (req, res) => {
 
     res.json(updatedDocument);
 
-  } catch
-    (err) {
+  } catch (err) {
     console.log(err);
     res.status(500).json({
       message: 'Unable to get posts'
@@ -51,11 +61,7 @@ export const getOne = async (req, res) => {
 export const create = async (req, res) => {
   try {
     const doc = new PostModel({
-      title: req.body.title,
-      text: req.body.text,
-      tags: req.body.tags,
-      imageUrl: req.body.imageUrl,
-      user: req.userId,
+      title: req.body.title, text: req.body.text, tags: req.body.tags, imageUrl: req.body.imageUrl, user: req.userId,
     });
 
     const post = await doc.save();
@@ -103,11 +109,7 @@ export const update = async (req, res) => {
     const updatedDocument = await PostModel.updateOne({
       _id: postId
     }, {
-      title: req.body.title,
-      text: req.body.text,
-      imageUrl: req.body.imageUrl,
-      user: req.userId,
-      tags: req.body.tags
+      title: req.body.title, text: req.body.text, imageUrl: req.body.imageUrl, user: req.userId, tags: req.body.tags
     });
 
     res.json({
